a4e98935e7f3f669a85f06bc199b5a2c84ad8477
nicolascwy <nicolas.chang99@gmail.com>
2023-09-04 20:19:24 +0800

    Create user endpoint

--- user-service/app.ts
+++ user-service/app.ts
@@ -3,8 +3,21 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  res.send('Hear you loud and clear');
+});
+
+// Create new user endpoint
+app.post('/user', (req, res) => {
+  if ('username' in req.body && 'password' in req.body && 'email' in req.body) {
+    res.send(
+      `Username: ${req.body.username} Password: ${req.body.password} Username: ${req.body.email}`
+    );
+  } else {
+    res.send("I don't know who the hell you are");
+  }
 });
 
 app.listen(port, () => {
