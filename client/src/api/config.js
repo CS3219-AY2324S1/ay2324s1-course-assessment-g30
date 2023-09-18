4ecea4c190e160b45a71511be0b511074aad8d18
Benjy Tan <71542870+benjytan45678@users.noreply.github.com>
2023-09-18 16:10:48 +0800

    Revert "Updated"

--- client/src/api/config.js
+++
@@ -1,4 +0,0 @@
-export const questionServiceURL = "http://localhost:3001/api";
-
-export const authServiceURL = "http://localhost:3000/v1";
-
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ChakraBaseProvider } from '@chakra-ui/react';
 
-
-ReactDOM.render(
-  <ChakraProvider>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <ChakraBaseProvider>
+      <App />
+    </ChakraBaseProvider>
   </React.StrictMode>
-  </ChakraProvider>,
-  document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
