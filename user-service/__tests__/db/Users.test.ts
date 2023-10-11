6076aa5a59460e54dd1bb556cc09533b94844d79
nicolascwy <nicolas.chang99@gmail.com>
2023-10-11 23:48:49 +0800

    User-service: Add DB unit test

---
+++ user-service/__tests__/db/Users.test.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import { UserDb } from '../../src/db/Users';
+
+const adminEmail = 'testAdmin1@test.com';
+const adminUsername = 'testAdmin1';
+const adminFirstName = 'adminTester';
+const lastName = 'testerLastName';
+const password = 'Testpassword1@';
+
+beforeEach(async () => {
+  await UserDb.initializeUserDatabase();
+});
+
+afterEach(async () => {
+  await UserDb.dropUserTable();
+});
+
+describe('User DB Unit test', () => {
+  test('admin peristed in DB', async () => {
+    await UserDb.createAdmin(
+      adminUsername,
+      adminFirstName,
+      lastName,
+      password,
+      adminEmail
+    );
+
+    const searchResult = UserDb.getUser({
+      email: adminEmail,
+      username: adminUsername,
+      firstName: adminFirstName,
+      lastName
+    });
+
+    expect(searchResult).not.toBeNull();
+  });
+});
