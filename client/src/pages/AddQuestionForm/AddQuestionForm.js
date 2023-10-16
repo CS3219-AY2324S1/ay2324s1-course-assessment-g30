459a97d3439be653d49545a84d0bb0f991b107b4
loyhongshenggg <e0725346@u.nus.edu>
2023-10-16 17:12:57 +0800

    Changed add question

--- client/src/pages/AddQuestionForm/AddQuestionForm.js
+++ client/src/pages/AddQuestionForm/AddQuestionForm.js
@@ -1,97 +1,165 @@
-import { Box, Button, Container, Toast, Spinner, Divider, FormControl, Heading, Input, Radio, RadioGroup, Stack, Text, Textarea, useToast } from '@chakra-ui/react';
-import React, {useState} from 'react';
+import { Box, Button, Container, Toast, Spinner, Divider, FormControl, Heading, Input, Radio, RadioGroup, Stack, Text, Textarea, useToast, Tooltip } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
 import { addQuestion } from '../../api/QuestionServices';
 
 function AddQuestionForm() {
-
-  //question ID auto generated
-  // Question Title Question Description Question Category Question Complexit
   const {
     register,
     handleSubmit,
     control,
-    formState: { errors }
+    formState: { errors },
+    setError,
+    clearErrors
   } = useForm();
 
-  let navigator = useNavigate()
-
+  const navigator = useNavigate();
   const toast = useToast();
   const [loading, setLoading] = useState(false);
+  const [questionLink, setQuestionLink] = useState('');
+  const [questionDescription, setQuestionDescription] = useState('');
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     setLoading(true);
-    addQuestion(data.title, data.categories, data.complexity, data.link, data.description)
-    .then(() => {navigator('/dashboard'); setLoading(false)})
-    .catch((e) => {
-      toast({
-        title: 'Unable to submit',
-        description: 'Please use a valid link',
-        status: 'warning',
-        duration: 4000,
-        isClosable: true,
-      })
+    console.log(data)
+    if (!data.link && !data.description) {
+      setError("link", {
+        type: "manual",
+        message: "Please provide a question link or description",
+      });
+
+      setError("description", {
+        type: "manual",
+        message: "Please provide a question link or description",
+      });
+
       setLoading(false);
-    }); 
-    };
-  // const onSubmit = data => {addQuestion(data.title, data.categories, data.complexity, data.link, data.description); navigator('/dashboard')};
+    } else {
+      clearErrors("link");
+      clearErrors("description");
+
+      addQuestion(data.title, data.categories, data.complexity, data.link, data.description)
+        .then(() => {
+          navigator('/dashboard');
+          setLoading(false);
+        })
+        .catch((e) => {
+          console.log(e)
+          toast({
+            title: 'Unable to submit',
+            description: 'Please use a valid leetcode link',
+            status: 'warning',
+            duration: 4000,
+            isClosable: true,
+          });
+          setLoading(false);
+        });
+    }
+  }
+
+  const handleLinkChange = (e) => {
+    setQuestionLink(e.target.value);
+    if (e.target.value) {
+      setQuestionDescription('');
+    }
+  };
+
+  const handleDescriptionChange = (e) => {
+    setQuestionDescription(e.target.value);
+    if (e.target.value) {
+      setQuestionLink('');
+    }
+  };
 
   return (
-    
-    <Box >
+    <Box>
       <Container maxW={'60%'} pb={20}>
         <Heading>Add Question</Heading>
       </Container>
 
       <Container maxW={'60%'} mb={100}>
-
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Text mb='20px' fontSize={'lg'} fontWeight={'semibold'}>Question Title</Text>
-          <Input {...register("title", { required: true })}/>
-          {errors.title && <p style={{color: 'red'}}>This field is required</p>}
+          <Text mb="20px" fontSize={'lg'} fontWeight={'semibold'}>
+            Question Title
+          </Text>
+          <Input {...register("title", { required: true })} />
+          {errors.title && <p style={{ color: 'red' }}>This field is required</p>}
           <Divider my={10} />
-          <Text mb='20px' fontSize={'lg'} fontWeight={'semibold'}>Question Category</Text>
-          <Input {...register("categories", { required: true })}/>
-          {errors.categories && <p style={{color: 'red'}}>This field is required</p>}
+          <Text mb="20px" fontSize={'lg'} fontWeight={'semibold'}>
+            Question Category
+          </Text>
+          <Input {...register("categories", { required: true })} />
+          {errors.categories && <p style={{ color: 'red' }}>This field is required</p>}
           <Divider my={10} />
-          <Text mb='20px' fontSize={'lg'} fontWeight={'semibold'}>Question Complexity</Text>
-          
+          <Text mb="20px" fontSize={'lg'} fontWeight={'semibold'}>
+            Question Complexity
+          </Text>
+
           <FormControl>
-          <Controller
+            <Controller
               control={control}
               rules={{ required: "This field is required" }}
               name="complexity"
               defaultValue={"Easy"}
               render={({ field }) => (
-          <RadioGroup {...field} >
-            <Stack direction='row' spacing={16}>
-              <Radio size='md' value='Easy'>Easy</Radio>
-              <Radio size='md' value='Medium'>Medium</Radio>
-              <Radio size='md' value='Hard'>Hard</Radio>
-            </Stack>
-          </RadioGroup>
+                <RadioGroup {...field}>
+                  <Stack direction='row' spacing={16}>
+                    <Radio size='md' value='Easy'>Easy</Radio>
+                    <Radio size='md' value='Medium'>Medium</Radio>
+                    <Radio size='md' value='Hard'>Hard</Radio>
+                  </Stack>
+                </RadioGroup>
               )}
-          />
+            />
           </FormControl>
-          
+
           <Divider my={10} />
-          <Text mb='20px' fontSize={'lg'} fontWeight={'semibold'}>Question link</Text>
-          <Input {...register("link", { required: true })}/>
-          {errors.link && <p style={{color: 'red'}}>This field is required</p>}
+          <Text mb="20px" fontSize={'lg'} fontWeight={'semibold'}>
+            Question Link
+          </Text>
+          {questionDescription.trim() ? (
+            <Tooltip hasArrow label="This field is disabled because the Question Description has content">
+              <Input
+                {...register("link")}
+                disabled
+              />
+            </Tooltip>
+          ) : (
+            <Input
+              {...register("link")}
+              onChange={handleLinkChange}
+              value={questionLink}
+            />
+          )}
+          {errors.link && <p style={{ color: 'red' }}>Either Question Link or Question Description must be filled</p>}
           <Divider my={10} />
-          <Text mb='20px' fontSize={'lg'} fontWeight={'semibold'}>Question Description</Text>
-          <Textarea {...register("description")}/>
+          <Text mb="20px" fontSize={'lg'} fontWeight={'semibold'}>
+            Question Description
+          </Text>
+          {questionLink.trim() ? (
+            <Tooltip hasArrow label="This field is disabled because the Question Link has content">
+              <Textarea
+                {...register("description")}
+                disabled
+              />
+            </Tooltip>
+          ) : (
+            <Textarea
+              {...register("description")}
+              onChange={handleDescriptionChange}
+              value={questionDescription}
+            />
+          )}
+          {errors.link && <p style={{ color: 'red' }}>Either Question Link or Question Description must be filled</p>}
           <Box display={'flex'} justifyContent={'flex-end'} py={16}>
-          {!loading && <Button type='submit' >Submit</Button>}
-          {loading && <Spinner />}
+            {!loading && <Button type='submit'>Submit</Button>}
+            {loading && <Spinner />}
           </Box>
         </form>
       </Container>
-    
-
     </Box>
-  )
+  );
 }
 
-export default AddQuestionForm
+export default AddQuestionForm;
