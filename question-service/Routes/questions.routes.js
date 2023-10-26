d216745f0143d79805b7fd42d7b72434b543cc5d
benjytan45678 <Benjytan45678@gmail.com>
2023-10-26 14:32:47 +0800

    update-route

--- question-service/Routes/questions.routes.js
+++ question-service/Routes/questions.routes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const questionController = require("../Controllers/questions.controller");
 const {
+  checkAuth,
+  checkUpdateResourceAuth,
+} = require("../Middleware/checkAuth.middleware.js");
+const {
   checkDuplicateQuestion,
 } = require("../Middleware/checkDuplicateQuestion.middleware.js");
 
@@ -8,31 +12,41 @@ const questionRouter = express.Router();
 
 questionRouter.post(
   "/readQuestionDescription",
+  checkAuth,
   questionController.readQuestionDescriptionController,
 );
 questionRouter.post(
   "/readQuestions",
+  checkAuth,
   questionController.readQuestionsController,
 );
 questionRouter.post(
   "/addQuestion",
+  checkAuth,
   checkDuplicateQuestion,
   questionController.addQuestionController,
 );
 questionRouter.post(
   "/readRandomQuestion",
+  checkAuth,
   questionController.readRandomQuestionController,
 );
 questionRouter.delete(
   "/deleteQuestion",
+  checkAuth,
+  checkUpdateResourceAuth,
   questionController.deleteQuestionController,
 );
 questionRouter.put(
   "/updateQuestion",
+  checkAuth,
+  checkUpdateResourceAuth,
   questionController.updateQuestionController,
 );
 questionRouter.post(
   "/testUpdateQuestion",
+  checkAuth,
+  checkUpdateResourceAuth,
   questionController.testUpdateQuestionController,
 );
 
